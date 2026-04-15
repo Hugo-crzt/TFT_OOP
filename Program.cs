680dcded090cd63ecf290202e130d27266118697
Crouzet <hugocrouz2a13@gmail.com>
2026-04-15 16:28:16 +0200

    calcul des synergies et creation du banc

--- Program.cs
+++ Program.cs
@@ -5,10 +5,13 @@ lulu.resourceBar.AfficherResourceBar();
 Garen garen = new Garen();
 garen.resourceBar.AfficherResourceBar();
 
-Map map = new Map(7,6);
+Map map = new Map();
+
 
 lulu.X = 3;
 lulu.Y = 3;
 //test pour voir si la map fonctionne bien 
 map.PlaceChampion(lulu,lulu.X,lulu.Y);
 map.display();
+
+Console.Write(GameManager.nbNinja);
